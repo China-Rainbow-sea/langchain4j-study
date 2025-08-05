5d188822733ad04cfc9c5a052484e68355582f63
RainbowSea <2969377330@qq.com>
2025-08-05 11:04:44 +0800

    学习: - LangChain4j之检索增强生成 RAG

---
+++ langchain4j-studys/langchain4j-12chat-rag/src/main/java/com/rainbowsea/langchain4jchatrag/ChatRAGLangChain4JApp.java
@@ -0,0 +1,15 @@
+package com.rainbowsea.langchain4jchatrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ */
+@SpringBootApplication
+public class ChatRAGLangChain4JApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ChatRAGLangChain4JApp.class,args);
+    }
+}
---
+++ langchain4j-studys/langchain4j-12chat-rag/src/main/java/com/rainbowsea/langchain4jchatrag/service/ChatAssistant.java
@@ -0,0 +1,14 @@
+package com.rainbowsea.langchain4jchatrag.service;
+
+/**
+ */
+public interface ChatAssistant {
+
+    /**
+     * 聊天
+     *
+     * @param message 消息
+     * @return {@link String }
+     */
+    String chat(String message);
+}
